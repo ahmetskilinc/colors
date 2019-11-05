c288852cbede10d8f9b3bb6ade15323ee963d786
ahmetskilinc <37756565+ahmetskilinc@users.noreply.github.com>
2019-11-05 18:08:34 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,21 +1,5 @@
-var hex = [
-	'0',
-	'1',
-	'2',
-	'3',
-	'4',
-	'5',
-	'6',
-	'7',
-	'8',
-	'9',
-	'a',
-	'b',
-	'c',
-	'd',
-	'e',
-	'f'
-];
+var hex = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+var colorHex = document.getElementById('colorhex');
 
 function gimmeColor() {
 	var color = '#';
@@ -25,14 +9,12 @@ function gimmeColor() {
 	}
 	var n_match = ntc.name(color);
 	n_name = n_match[1];
-	document.getElementById('colorhex').style.display = 'block';
-	document.getElementById('colorhex').innerHTML = color + ' - ' + n_name;
-	document.getElementById('colorbox').style.display = 'block';
-	document.getElementById('colorbox').style.background = color;
-	document.getElementById('colorbox').style.boxShadow =
-		'0px 10px 68px -10px rgba(0,0,0,0.75)';
+	colorHex.style.display = 'block';
+	colorHex.innerHTML = color + ' - ' + n_name;
+	colorHex.style.display = 'block';
+	colorHex.style.background = color;
+	colorHex.style.boxShadow = '0px 10px 68px -10px rgba(0,0,0,0.75)';
 	document.getElementById('button').style.color = color;
-	document.getElementById('colorhex').style.color = color;
-	document.title = color;
-	console.log(color);
+	colorHex.style.color = color;
+// 	console.log(color);
 }
