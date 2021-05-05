0760d8fd78056c4d4411b641fc3a3602551f30d7
Ahmet Kilinc <akx9@icloud.com>
2021-05-05 01:45:14 +0100

    streamline code

--- scripts.js
+++ scripts.js
@@ -10,17 +10,19 @@ function newColour() {
 		Math.floor(Math.random() * 16777215)
 			.toString(16)
 			.toUpperCase();
-	const colourName = ntc.name(colour)[1];
-	document.body.style.background = colour;
-	document.getElementById("colourOutput").innerHTML = "Name: " + colourName + `<br/>` + "Hex: " + colour;
-	window.document.title = colourName + " - " + colour;
+	getColourAndChangeShit(colour);
 }
 
 function getHexName() {
 	const colour = "#" + document.getElementById("hexInput").value;
 	console.log(colour);
 	const colourName = ntc.name(colour)[1];
+	getColourAndChangeShit(colour);
+}
+
+function getColourAndChangeShit(colour) {
+	const colourName = ntc.name(colour)[1];
 	document.body.style.background = colour;
-	document.getElementById("nameOutput").innerHTML = "Name: " + colourName + `<br/>` + "Hex: " + colour;
+	document.getElementById("colourOutput").innerHTML = "Name: " + colourName + `<br/>` + "Hex: " + colour;
 	window.document.title = colourName + " - " + colour;
 }
