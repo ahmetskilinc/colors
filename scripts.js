117342119c878943fe586c19f71ecda6df2876bc
Ahmet Kilinc <akx9@icloud.com>
2021-05-07 12:39:22 +0100

    update

--- scripts.js
+++ scripts.js
@@ -1,26 +1,27 @@
 document.body.addEventListener("keyup", function (e) {
-	if (e.keyCode == 13) {
-		newColour();
+	if (e.key === "Enter" || e.code === "Enter") {
+		runNewColour(true);
 	}
 });
 
-function newColour() {
-	const colour =
-		"#" +
-		Math.floor(Math.random() * 16777215)
-			.toString(16)
-			.toUpperCase();
-	getColourAndChangeShit(colour);
-}
+const runNewColour = (get) => {
+	let colour;
+	if (get) {
+		colour =
+			"#" +
+			Math.floor(Math.random() * 16777215)
+				.toString(16)
+				.toUpperCase();
+	} else {
+		colour = "#" + document.getElementById("hexInput").value;
+	}
+	getColourAndChangeShit(colour, get);
+};
 
-function getHexName() {
-	const colour = "#" + document.getElementById("hexInput").value;
-	getColourAndChangeShit(colour);
-}
-
-function getColourAndChangeShit(colour) {
+const getColourAndChangeShit = (colour, get) => {
 	const colourName = ntc.name(colour)[1];
 	document.body.style.background = colour;
-	document.getElementById("colourOutput").innerHTML = "Name: " + colourName + `<br/>` + "Hex: " + colour;
+	document.getElementById(get ? "colourOutput" : "nameOutput").innerHTML =
+		`<p>` + "Name: " + colourName + `</p><p>` + "Hex: " + colour + `</p>`;
 	window.document.title = colourName + " - " + colour;
-}
+};
